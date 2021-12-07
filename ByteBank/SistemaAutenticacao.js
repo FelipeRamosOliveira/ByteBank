45d0b7f6ea240e2642aacb552def601ddf6b67f6
FelipeOliveira <felipe.oliveira@kunumi.com>
2021-12-07 15:07:02 -0300

    Aula 6

--- ByteBank/SistemaAutenticacao.js
+++ ByteBank/SistemaAutenticacao.js
@@ -3,7 +3,16 @@ Ser autenticavel significa ter a propriedade senha e a funcao autenticar
 */
 
 export class SistemaAutenticacao {
-  static login(funcionario, senha) {
-    return funcionario.autenticar (senha);
+  static login(autenticavel, senha) {
+    if (SistemaAutenticacao.ehautenticavel(autenticavel)) {
+      return autenticavel.autenticar(senha);
+    }
+    return false;
+  }
+
+  static ehautenticavel(autenticavel) {
+    return (
+      autenticavel.autenticar && typeof autenticavel.autenticar === "function"
+    );
   }
 }
